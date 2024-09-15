e87eae5d55ad1090e09df2706443e52f69e2a537
Arti_code <117987641+Arthiar@users.noreply.github.com>
2024-09-15 12:12:30 +0200

    Update WordSort.java

--- WordSort.java
+++ WordSort.java
@@ -1,30 +1,15 @@
-
 import java.util.Arrays;
 
 public class WordSort {
     public static void main(String[] args) {
-        String text = "To be or not to be, that is the question; " +
-                      "Whether 'tis nobler in the mind to suffer " +
-                      "the slings and arrows of outrageous fortune, " +
-                      "or to take arms against a sea of troubles, " +
-                      "and by opposing end them?";
-
-        // Remove punctuation and split the text into words
+        String text = "To be or not to be, that is the question; Whether 'tis nobler in the mind to suffer the slings " +
+                      "and arrows of outrageous fortune, or to take arms against a sea of troubles, and by opposing end them?";
         String[] words = text.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\s+");
-
-        // Apply bubble sort to sort the words
         for (int i = 0; i < words.length - 1; i++) {
             for (int j = 0; j < words.length - i - 1; j++) {
                 if (words[j].compareTo(words[j + 1]) > 0) {
-                    // Swap words[j] and words[j + 1]
                     String temp = words[j];
                     words[j] = words[j + 1];
-                    words[j + 1] = temp;
-                }
-            }
-        }
+                    words[j + 1] = temp;  } }  }
+        System.out.println("Sorted words: " + Arrays.toString(words)); }}
 
-        // Output the sorted words
-        System.out.println("Sorted words: " + Arrays.toString(words));
-    }
-}
